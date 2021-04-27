d427bf3f20d5e9c43dfaa92b16da71f2792d9c0b
Raivydas Simenas <raivydas.simenas@icloud.com>
2021-04-27 17:35:11 +0300

    Functional Components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
-import logo from './logo.svg';
+import { DISHES } from './shared/dishes';
 import './App.css';
+import Main from './components/MainComponent';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu />
+        <Main />
       </div>
     );
   }
