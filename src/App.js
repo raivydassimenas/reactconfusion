fb1c338d1152cf661d79550e6a732d81ff2853b7
Raivydas Šimėnas <raivydas.simenas@icloud.com>
2021-05-06 15:22:40 +0300

    Intro to Redux

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import { BrowserRouter } from 'react-router-dom';
-import Menu from './components/MenuComponent';
-import { DISHES } from './shared/dishes';
 import './App.css';
 import Main from './components/MainComponent';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
+
 
 class App extends Component {
 
   render() {
     return (
+      <Provider store={store}>
       <BrowserRouter>
         <div className="App">
           <Main />
         </div>
       </BrowserRouter>
+      </Provider>
     );
   }
 }
