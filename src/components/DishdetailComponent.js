67b51a9bb2e0c01ee123e6322f2e6fc8f50552ce
Raivydas Šimėnas <raivydas.simenas@icloud.com>
2021-05-12 17:26:26 +0300

    Fetch Post Comments

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -29,7 +29,7 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
-        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render() {
@@ -101,8 +101,7 @@ function RenderDish({dish}) {
 }
 
 
-function RenderComments({comments, addComment, dishId}) { 
-    
+function RenderComments({comments, postComment, dishId}) {   
     if (comments != null) {
         return(
             <div>
@@ -117,7 +116,7 @@ function RenderComments({comments, addComment, dishId}) {
                         );
                     })}
                 </ul>
-                <CommentForm dishId={dishId} addComment={addComment}/>
+                <CommentForm dishId={dishId} postComment={postComment} />
             </div>
         );
     }
@@ -166,7 +165,7 @@ const  DishDetail = (props) => {
                 </div>
                 <div className="col-12 col-md-5 m-1">
                 <RenderComments comments={props.comments}
-                    addComment={props.addComment}
+                    postComment={props.postComment}
                     dishId={props.dish.id}
                 />
                 </div>
